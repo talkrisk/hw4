d759e1f0ec9306e66dbfef92b0ed68e567742943
talkrisk <sam.cooper@kellogg.northwestern.edu>
2022-06-08 01:07:59 +0000

    Phase 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["first_name"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
     @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
