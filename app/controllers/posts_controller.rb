33e4ca26c8ac1267f1ce79243e6bfcd914441aa3
talkrisk <sam.cooper@kellogg.northwestern.edu>
2022-06-08 00:21:18 +0000

    Phase 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,16 @@ class PostsController < ApplicationController
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
+  else
+    flash["notice"] = "Login first."
+  end
     redirect_to "/places/#{@post["place_id"]}"
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
   def create
     @user = User.new
     @user["first_name"] = params["user"]["first_name"]
     @user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/posts"
   end
 end
